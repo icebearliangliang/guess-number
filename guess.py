c0ef5b6bbeada02cbae5216accf87c1ecf9e7631
icebear <icebearliangliang@163.com>
2019-04-13 18:08:22 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,16 +1,19 @@
 import random
+start = input('请输入范围开始值: ')
+end = input('请输入范围结束值: ')
 r = random.randint(1, 100)
 count=0
 while True:
-	num = input('please guess number: ')
+	num = input('请猜数字: ')
 	num = int(num)
 	count = count + 1 # count += 1
 	if num == r:
-		print('you are right!')
-		print('this is the', count, 'time')
+		print('你猜对了!')
+		print('这是你猜的第', count, '次')
 		break
 	elif num > r:
 		print('too big')
 	elif num < r:
 		print('too small')
-	print('this is the', count, 'time')
+	print('这是你猜的第', count, '次')
+		
